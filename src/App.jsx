461b91eda008c570d63ddaf49cef758a4319a412
Marcos Mantovani <mantovani.marcos@gmail.com>
2025-02-13 18:29:55 -0300

    feat: integrate react-router-dom for navigation and add routing for menu, cart, and order components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import Cart from '@/features/cart/Cart'
+import Menu from '@/features/menu/Menu'
+import CreateOrder from '@/features/order/CreateOrder'
+import Order from '@/features/order/Order'
+import Home from '@/ui/Home'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/menu',
+    element: <Menu />
+  },
+  {
+    path: '/cart',
+    element: <Cart />
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder />
+  },
+  {
+    path: '/order/:orderId',
+    element: <Order />
+  }
+])
+
 function App() {
-  return <div>App.jsx</div>
+  return <RouterProvider router={router} />
 }
 
 export default App
