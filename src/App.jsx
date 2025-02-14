244e6108ad6558650cf27e235795972b0fd9be66
Marcos Mantovani <mantovani.marcos@gmail.com>
2025-02-14 12:29:59 -0300

    feat: configure Tailwind CSS with PostCSS and Prettier integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Cart from '@/features/cart/Cart'
-import Menu, { loader as menuLoader } from '@/features/menu/Menu'
-import CreateOrder, { action as createOrderAction } from '@/features/order/CreateOrder'
-import Order, { loader as orderLoader } from '@/features/order/Order'
-import AppLayout from '@/ui/AppLayout'
-import Home from '@/ui/Home'
-import Error from '@/ui/Error'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Cart from '@/features/cart/Cart';
+import Menu, { loader as menuLoader } from '@/features/menu/Menu';
+import CreateOrder, {
+  action as createOrderAction,
+} from '@/features/order/CreateOrder';
+import Order, { loader as orderLoader } from '@/features/order/Order';
+import AppLayout from '@/ui/AppLayout';
+import Home from '@/ui/Home';
+import Error from '@/ui/Error';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
@@ -17,26 +19,26 @@ const router = createBrowserRouter([
         path: '/menu',
         element: <Menu />,
         loader: menuLoader,
-        errorElement: <Error />
+        errorElement: <Error />,
       },
       { path: '/cart', element: <Cart /> },
       {
         path: '/order/new',
         element: <CreateOrder />,
-        action: createOrderAction
+        action: createOrderAction,
       },
       {
         path: '/order/:orderId',
         element: <Order />,
         loader: orderLoader,
-        errorElement: <Error />
-      }
-    ]
-  }
-])
+        errorElement: <Error />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
