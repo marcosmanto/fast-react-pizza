3a063937e760835a7023fc521bc1c20230206b1d
Marcos Mantovani <mantovani.marcos@gmail.com>
2025-02-19 17:56:01 -0300

    feat: add framer-motion for animations in CartOverview component

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,14 +1,42 @@
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
 function CartOverview() {
+  const [isAnimating, setIsAnimating] = useState(true);
+
+  const cart = useSelector((state) => state.cart.cart);
+  const numPizzas = cart.length;
+
+  useEffect(() => {
+    document.body.style.overflow = isAnimating ? 'hidden' : '';
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [isAnimating]);
+
+  if (numPizzas === 0) return;
+
   return (
-    <div className="flex items-center justify-between bg-stone-800 p-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        ease: 'easeOut',
+        duration: 0.5,
+        onComplete: () => setIsAnimating(false),
+      }}
+      className="flex items-center justify-between bg-stone-800 p-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base"
+    >
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span>23 pizzas</span>
+        <span>
+          {cart.length} pizza{cart.length > 1 && 's'}
+        </span>
         <span>$23.45</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
-    </div>
+    </motion.div>
   );
 }
 
